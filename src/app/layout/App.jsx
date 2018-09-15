41e81463b984046d040bc387e544c6514dd1174a
Hammad Ali <hammadali116@gmail.com>
2018-09-16 03:54:32 +0500

    Navbar Fixed Routing Added

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
-import NavBar from "../../features/nav/NavBar";
+import NavBar from "../../features/nav/navBar/NavBar";
+import { Route, Switch } from "react-router-dom";
+import EventDetailedPage from "../../features/event/eventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/peopleDashboard/PeopleDashboard";
+import UserDetailedPage from "../../features/user/userDetailed/UserDetailedPage";
+import SettingsDashboard from "../../features/user/settings/SettingsDashboard";
+import EventForm from "../../features/event/eventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
+
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css'
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+import App from "./app/layout/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter } from "react-router-dom";
+const rootEl = document.getElementById("root");
 
-const rootEl =document.getElementById('root');
+let render = () => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
+};
 
-let render= () =>{
-    ReactDOM.render(<App />, rootEl);    
-}
-
-if(module.hot){
-    module.hot.accept('./app/layout/App',()=>{
-        setTimeout(render);
-    })
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 render();
 
